54aa599d0515a0228033b211e89363ff7b64da85
wesamsaleh <wesamssaleh@gmail.com>
2021-10-27 00:01:55 -0400

    Added all necessary UI elements and imported the Tuition classes

--- src/main/java/com/example/project3/Main.java
+++ src/main/java/com/example/project3/Main.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 550);
+        stage.setTitle("Tuition Manager");
         stage.setScene(scene);
         stage.show();
     }
